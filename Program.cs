f44c6bdf8cd16def4c671c3d1bfeea0cb34a3428
BrianYannLee <165946084+BrianYannLee@users.noreply.github.com>
2024-05-20 22:02:59 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿//Programmer: Brian Lee
+// Date: 04/02/24
+
+//Title: CSI 120 class Week 1 T
+
+
+
+namespace CSI_120_Blee_04_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello,Test World!");// Copy and paste just Ctrl C/V the line
+            Console.WriteLine();
+            Console.Write("Hello,");  //Start next code in Same line
+            Console.WriteLine("Test World!"); //Start next code in next line
+
+
+        } // End of Main
+    } //End of Class
+} //End of Namespace
+
+
+
+
+/* Notes
+ * Everything runs of Javascript. Javascript is a dynamically typed scripting langauge primarily used for web devleopment, while Java is a statically typed programming language commonly utilized for building enterprise-level applications and Android mobile apps.
+ * 
+ * Python is really good in math. C# use for gaming
+ * 
+ * A Framework is a pre-built, reusable set of code libraries, compontnet, tools, and conventions that provide a foundation for developming software applicatoins
+ * 
+ * C# is a compile type of language. It convert in to binary. This would lead to Compile Time Error.
+ * 
+ * Comment after close curly brace to show the end of something. 
+ * Curly brace is are sections of code blocks.
+ * 
+ * Print is in Python. Writeline is C#
+ * 
+ * Questions:
+ * How to save different versions of code efficiently?
+ * What is the purpose of read me?
+ * 
+ */
